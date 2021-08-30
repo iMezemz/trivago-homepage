ee968cc99a99d2bc05515cff7b966ceeecd41768
iMezemz <iMezemz@users.noreply.github.com>
2021-08-30 07:13:17 +0200

    combined FaqList and MessageWindow components into one for easier control of display style

---
+++ src/components/MainWindow.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="main-div">
+    <FaqList title="Bookings" />
+    <MessageWindow />
+  </div>
+</template>
+
+<script>
+import MessageWindow from "./MessageWindow.vue";
+import FaqList from "./FaqList.vue";
+export default {
+  components: { MessageWindow, FaqList },
+};
+</script>
+
+<style scoped>
+#main-div {
+  display: flex;
+  justify-content: space-between;
+}
+@media only screen and (max-width: 1045px) {
+  #main-div {
+    flex-direction: column;
+  }
+}
+</style>
