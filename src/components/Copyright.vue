b502353af5b4af7274da8f120a49134cb2f737e7
iMezemz <iMezemz@users.noreply.github.com>
2021-08-30 07:09:00 +0200

    done with footer of webpage

---
+++ src/components/Copyright.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <img
+      src="https://theme.zdassets.com/theme_assets/196898/39fabd63b675f6a86643e6ae1f6b3d5ed3ce4bab.svg"
+      alt=""
+    />
+    <p>Copyright 2021 trivago | All rights reserved.</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(55, 69, 77);
+  padding-top: 2%;
+}
+p {
+  color: rgb(151, 161, 163);
+}
+img {
+  height: 32px;
+  width: auto;
+}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,72 @@
+<template>
+  <div
+    style="
+      background-color: rgb(41, 52, 58);
+      margin: 0;
+      padding: 2% 15%;
+      margin-top: 4%;
+    "
+  >
+    <div style="display: flex; justify-content: space-between">
+      <p>trivago N.V., Kesselstraße 5 – 7, 40221 Düsseldorf, Germany</p>
+      <span style="border: 1px solid white">
+        <p>English (US)</p>
+      </span>
+    </div>
+    <!-- <div>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div> -->
+    <div id="links-wrapper">
+      <ul>
+        <li>Company</li>
+        <li>Jobs</li>
+        <li>Press</li>
+        <li>Investor Relations</li>
+      </ul>
+      <ul>
+        <li>Mobile Apps - Search on the go</li>
+        <li>trivago Business Studio</li>
+        <li>trivago Magazine</li>
+      </ul>
+      <ul>
+        <li>Terms and Conditions</li>
+        <li>Legal Information</li>
+        <li>Privacy Policy</li>
+        <li>Cookie Policy</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+li,
+p {
+  color: white;
+}
+span {
+  min-width: 25%;
+  padding-left: 2%;
+}
+ul {
+  list-style: none;
+  margin-right: 6%;
+}
+#links-wrapper {
+  display: flex;
+  justify-content: flex-start;
+}
+li:hover {
+  text-decoration: underline;
+  cursor: pointer;
+}
+</style>
