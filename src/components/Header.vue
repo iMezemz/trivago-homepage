292d1202c8a37e4f31c623b63e082b3e66edbfa8
iMezemz <iMezemz@users.noreply.github.com>
2021-08-30 07:09:44 +0200

    added header vue component

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <img
+      src="https://theme.zdassets.com/theme_assets/196898/9addc16d02ae592b532d01410947d206e21ac2bf.png"
+    />
+    <ul>
+      <li>Home</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+  height: 110px;
+  width: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 0 5%;
+}
+img {
+  width: 155px;
+  height: 64px;
+}
+li {
+  display: flex;
+  align-items: center;
+}
+li:hover {
+  text-decoration: underline;
+  cursor: pointer;
+}
+</style>
